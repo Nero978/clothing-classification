0b9c7219e74cbfe1f9c2fc8bb2fd3baebb098883
Nero978 <zhumengfeiyang@qq.com>
2025-01-03 23:07:12 +0800

    feat: 添加标签映射功能并优化数据集加载

---
+++ map.py
@@ -0,0 +1,31 @@
+label_map = {
+    0: "T-Shirt",
+    1: "Long Sleeve",
+    2: "Pants",
+    3: "Shoes",
+    4: "Shirt",
+    5: "Dress",
+    6: "Outwear",
+    7: "Shorts",
+    8: "Hat",
+    9: "Skirt",
+    10: "Polo",
+    11: "Undershirt",
+    12: "Blazer",
+    13: "Hoodie",
+    14: "Body",
+    15: "Top",
+    16: "Blouse",
+    17: "Not sure",
+    18: "Others",
+    19: "Skip",
+}
+
+def get_label_id(label):
+    for key, value in label_map.items():
+        if value == label:
+            return key
+    return None
+
+def get_label_name(label_id):
+    return label_map[label_id]
